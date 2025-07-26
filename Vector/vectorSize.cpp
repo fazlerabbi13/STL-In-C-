e11e5bf38aa0d8bd4b8d2e6cafe1b6d7cfcfeb8a
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-26 21:50:12 +0600

    One of the common problems with arrays was to keep a separate variable to store the size information. Vector provides the solution to this problem by providing size() method.

---
+++ Vector/vectorSize.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<char> v = {'a', 'c', 'f', 'd', 'z'};
+
+    // Finding size
+    cout << v.size();
+  
+    return 0;
+}
