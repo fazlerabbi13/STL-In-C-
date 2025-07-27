fd2b7ee45925d9c5e998b51f18afa416b1e6a618
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-27 22:02:37 +0600

    Deque can be traversed by simply using a loop in which each element is accessed using its index. In deque, the indexes start from 0 and goes till deque.size() - 1. where the function size() returns the current number of elements in the deque.

---
+++ Deque/TraverseInDeque.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    deque<int> dq = {1, 4, 2, 3, 5};
+
+    // Traversing using the for loop
+    for (int i = 0; i < dq.size(); i++) 
+        cout << dq[i] << " ";
+        
+    return 0;
+}
