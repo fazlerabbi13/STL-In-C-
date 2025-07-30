b95983f0afe3c87692a9177e2540603ce81ec2c5
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-30 22:00:01 +0600

    Creating a forward_list with default value.

--- ForwardList/declaration.cpp
+++ ForwardList/declaration.cpp
@@ -14,13 +14,13 @@ int main() {
 
     // Creating a forward_list with
     // default value
-    // forward_list<int> fl2(3, 4);
+     forward_list<int> fl2(3, 4);
     
     // Creating a forward_list from an
     // initializer list
     // forward_list<int> fl3 = {1, 5, 3, 4};
     
-    // printFL(fl2);
+     printFL(fl2);
     // printFL(fl3);
     return 0;
 }
