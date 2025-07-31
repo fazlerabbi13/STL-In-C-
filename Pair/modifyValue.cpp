e2d94cbf933028315ec8d8c2516ee6667259ac09
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-31 20:50:36 +0600

    We update the elements of pair like accessing elements from pair but in place of access, we just assign new value using assignment operator.

---
+++ Pair/modifyValue.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<int, string> p = {1, "Geeks"};
+
+    // Update first and second value of pair
+    p.first = 2;
+    p.second = "ForGeeks";
+    cout << p.first << " " << p.second;
+    return 0;
+}
