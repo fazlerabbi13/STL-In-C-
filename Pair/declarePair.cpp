ee97fbf3035b083e975704c728471d9475ddeac4
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-31 20:36:37 +0600

    Creating an empty pair

---
+++ Pair/declarePair.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+// Creating an empty pair
+pair<int, string> p1;
+
+// Insert values using curly braces {}
+// pair<int, string> p2 = {1, "Geeks"};
+
+// Insert values using make_pair method
+// pair<int, string> p3 = make_pair(2, "ForGeeks");
+
+// cout << p2.first << " " << p2.second << endl;
+// cout << p3.first << " " << p3.second;
+return 0;
+}
