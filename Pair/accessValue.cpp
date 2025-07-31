afcf835b7a2327c806bab78e455b9bed5a9d5d55
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-31 20:41:22 +0600

    In pair, first and second values are stored as data members. So, we can access them by using their name with (.) operator.

---
+++ Pair/accessValue.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<int, string> p = {1, "Geeks"};
+
+    // Accessing the elements of the pair
+    cout << p.first << " " << p.second;
+    
+    return 0;
+}
