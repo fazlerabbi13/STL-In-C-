28ac27e0b3a4f3dbfedaaae1e8098cb13555958f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-02 20:33:50 +0600

    Create an empty unordered_set.

---
+++ unorderedSets/declaration.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // Create an empty unordered_set
+    unordered_set<int> us1;
+  
+    // Initialize an unordered_set using
+    // using intializer list
+    // unordered_set<int> us2 = {1, 2, 3, 4, 5};
+    
+    // for(auto x : us2)
+    //     cout << x << " ";
+    return 0;
+}
