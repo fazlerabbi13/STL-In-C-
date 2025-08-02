023d82e11240521e0fc723650b653efe9c8329b8
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-02 20:35:19 +0600

    Initialize an unordered_set using intializer list.

--- unorderedSets/declaration.cpp
+++ unorderedSets/declaration.cpp
@@ -8,9 +8,9 @@ int main() {
   
     // Initialize an unordered_set using
     // using intializer list
-    // unordered_set<int> us2 = {1, 2, 3, 4, 5};
+    unordered_set<int> us2 = {1, 2, 3, 4, 5};
     
-    // for(auto x : us2)
-    //     cout << x << " ";
+    for(auto x : us2)
+        cout << x << " ";
     return 0;
 }
