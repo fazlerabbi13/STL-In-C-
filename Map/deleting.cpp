0e2a74fd2f410906b4d127abaf5a288bb4ee1683
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-06 21:21:16 +0600

    create a map and delete element using erase() function by key.

---
+++ Map/deleting.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<int, string> m = {{1, "Geeks"},
+             {2, "For"}, {3, "Geeks"}};
+
+    // Deleting by key
+    m.erase(2);
+    
+    // // Deleting by iterator
+    // m.erase(m.begin());
+    
+    for(auto i : m)
+        cout << i.first << " " << i.second
+        << endl;
+    return 0;
+}
