25622d2e8409541c5f3a992ce8c20f820a38388e
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-06 21:07:27 +0600

    Initialze map with list and iterate it.

--- Map/declaration.cpp
+++ Map/declaration.cpp
@@ -6,12 +6,12 @@ int main() {
     // Creating an empty map
     map<int, string> m1;
 
-    // // Initialze map with list
-    // map<int, string> m2 = {{1, "Geeks"},
-    //           {2, "For"}, {3, "Geeks"}};
+    // Initialze map with list
+    map<int, string> m2 = {{1, "Geeks"},
+              {2, "For"}, {3, "Geeks"}};
 
-    // for (auto& p : m2)
-    //     cout << p.first << " " <<
-    //     p.second << endl;
+    for (auto& p : m2)
+        cout << p.first << " " <<
+        p.second << endl;
     return 0;
 }
