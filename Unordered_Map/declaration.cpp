1f8f6cdf993209f073ad2cae3af60c54e888aa09
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-07 20:58:23 +0600

    Creating an unordered_map using.

--- Unordered_Map/declaration.cpp
+++ Unordered_Map/declaration.cpp
@@ -14,11 +14,11 @@ int main() {
     
     // Creating an unordered_map using
     // initializer list
-    // unordered_map<int, string> um2 =
-    // {{1, "Geeks"}, {2, "For"}, {3, "C++"}};
+    unordered_map<int, string> um2 =
+    {{1, "Geeks"}, {2, "For"}, {3, "C++"}};
     
     print(um1);
-    // cout << endl;
-    // print(um2);
+    cout << endl;
+    print(um2);
     return 0;
 }
