1d381e8249070e6ab838b05294dfe522cfa5d906
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-08 22:04:34 +0600

    Creating an empty set of integers.

---
+++ Multiset/declaration.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // Creating an empty set of integers
+    multiset<int> ms1;
+    
+    // Initialize with initializer list
+    // multiset<int> ms2 = {5, 3, 3, 1};
+
+    // for (auto i : ms2)
+    //     cout << i << " ";
+    return 0;
+}
