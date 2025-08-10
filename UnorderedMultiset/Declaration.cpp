3190f87ea2cd93b5c8ac22c2492596d4e916ec26
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-10 22:56:00 +0600

    Create an empty unordered multiset.

---
+++ UnorderedMultiset/Declaration.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // Create an empty unordered multiset
+    unordered_multiset<int> ums1;
+    
+    // Create and initialize elements
+    // using initialzer list
+    // unordered_multiset<int> ums2 =
+    //                       {5, 3, 4, 1, 1};
+    
+    // for(auto x : ums2)
+    //     cout << x << " ";;
+    return 0;
+}
