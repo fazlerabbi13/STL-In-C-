e0a28982e43801742b2cdca60aae329e8861c6a4
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-11 19:38:00 +0600

    Creating an empty unordered_multimap.

---
+++ UnorderedMultimap/Declaration.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // Creating an empty unordered_multimap
+    unordered_multimap<int, string> umm1;
+    
+    // Creating unordered_multimap using
+    // initializer list
+    //unordered_multimap<int, string> umm2 =
+    // {{1, "Geeks"}, {2, "For"}, {1, "C++"}};
+
+    // for (auto x : umm2) 
+    //     cout << x.first << ": " << x.second
+    //     << endl;
+    return 0;
+}
