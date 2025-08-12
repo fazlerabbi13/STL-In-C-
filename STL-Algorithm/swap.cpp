7606093e582eb040d4735d66566f85b212708222
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-12 22:10:04 +0600

    Exchanges the values of two variables.

---
+++ STL-Algorithm/swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a = 1, b = 55;
+    cout << a << " " << b << endl;
+    
+    // swapping the values of a and b
+    swap(a, b);
+    
+    cout << a << " " << b;
+    return 0;
+}
