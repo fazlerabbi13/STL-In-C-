4852bee8e7afd8246d9e7467713afdf1e15add26
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-16 20:54:22 +0600

    Insert values using make_pair method.

--- Utility/Pair.cpp
+++ Utility/Pair.cpp
@@ -7,12 +7,12 @@ int main() {
 pair<int, string> p1;
 
 // Insert values using curly braces {}
-//pair<int, string> p2 = {1, "Geeks"};
+pair<int, string> p2 = {1, "Geeks"};
 
 // Insert values using make_pair method
 //pair<int, string> p3 = make_pair(2, "ForGeeks");
 
-// cout << p2.first << " " << p2.second << endl;
+ cout << p2.first << " " << p2.second << endl;
 // cout << p3.first << " " << p3.second;
 return 0;
 }
