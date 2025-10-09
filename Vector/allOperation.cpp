5a62ebfa12ddf887cd63ce33fe1464890aebd2ae
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-09 15:29:40 +0600

    Exchanges the content of the container by the content of x, which is another vector object of the same type. Sizes may differ.

--- Vector/allOperation.cpp
+++ Vector/allOperation.cpp
@@ -3,30 +3,47 @@
 using namespace std;
 
 int main(){
-    vector<int> num {1,2,3,4,5,6,7,8,9};
+    vector<int> num1 {1,2,3,4,5,6,7,8,9};
+    vector<int> num2 {9,8,7,4,5,6,1,2,3};
+
+    num2.swap(num1);
+
+
+    for (int i = 0; i < num1.size(); i++)
+    {
+        cout << num1[i] << "\n";
+    }
+    
+    cout << "-------------" << "\n";
+
+    for (int i = 0; i < num2.size(); i++)
+    {
+        cout << num2[i] << "\n";
+    }
+    
     // cout << num.max_size()<<"\n";
     // num.shrink_to_fit();
     // cout << num.capacity();
 
     // cout << num.back() <<"\n";
 
-    for(auto it = num.cbegin(); it != num.cend(); ++it)
-        cout << *it << "\n";
+    // for(auto it = num.cbegin(); it != num.cend(); ++it)
+    //     cout << *it << "\n";
 
     // for(auto it = num.crbegin(); it !=num.crend(); ++it)
     //     cout << *it << "\n";
-    cout<<"----------"<<"\n";
+    // cout<<"----------"<<"\n";
 
     // num.emplace(num.begin()+2,100);
 
-    num.emplace_back(10);
-    num.emplace_back(11);
+    // num.emplace_back(10);
+    // num.emplace_back(11);
 
-    num.erase(num.begin()+1);
-    num.erase(num.begin()+1,num.begin()+2);
+    // num.erase(num.begin()+1);
+    // num.erase(num.begin()+1,num.begin()+2);
 
-    for(auto it = num.begin(); it != num.end(); ++it)
-        cout << *it << "\n";
+    // for(auto it = num.begin(); it != num.end(); ++it)
+    //     cout << *it << "\n";
 
     return 0;
 }
