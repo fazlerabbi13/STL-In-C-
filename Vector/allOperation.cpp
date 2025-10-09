219230abf87e6178f2ca4490e1a495c197c6de42
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-09 15:41:14 +0600

    Resizes the container so that it contains n elements.

--- Vector/allOperation.cpp
+++ Vector/allOperation.cpp
@@ -4,22 +4,26 @@ using namespace std;
 
 int main(){
     vector<int> num1 {1,2,3,4,5,6,7,8,9};
-    vector<int> num2 {9,8,7,4,5,6,1,2,3};
 
-    num2.swap(num1);
+    num1.resize(11);
+    num1.resize(15,100);
+    num1.resize(5);
+    // vector<int> num2 {9,8,7,4,5,6,1,2,3};
+
+    // num2.swap(num1);
 
 
     for (int i = 0; i < num1.size(); i++)
     {
         cout << num1[i] << "\n";
     }
-    
-    cout << "-------------" << "\n";
 
-    for (int i = 0; i < num2.size(); i++)
-    {
-        cout << num2[i] << "\n";
-    }
+    // cout << "-------------" << "\n";
+
+    // for (int i = 0; i < num2.size(); i++)
+    // {
+    //     cout << num2[i] << "\n";
+    // }
     
     // cout << num.max_size()<<"\n";
     // num.shrink_to_fit();
