e982918739c87fa78a6aef4c1a3a1db6259edca9
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-10 18:50:32 +0600

    assign() function  used to assign values to a list.

---
+++ List/allOperation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    list<int>li;
+    li.assign(5,3);
+    
+    for(int l: li){
+        cout << l <<" ";
+    }
+
+
+    return 0;
+}
