e83d742431a48137d0dde7c5d00adc25963e5b5f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-10 19:06:54 +0600

    Removes from the container all the elements that compare equal to val.

--- List/allOperation.cpp
+++ List/allOperation.cpp
@@ -6,6 +6,8 @@ int main(){
     list<int>li;
     list<int>li2 {5,2,3,4};
 
+    li2.remove(4);
+
     li.assign(5,3);
 
     li.emplace_back(7);
@@ -14,7 +16,9 @@ int main(){
     li.pop_front();
 
     li.splice(li.begin(),li2);
+
     
+
     for(int l: li){
         cout << l <<" ";
     }
