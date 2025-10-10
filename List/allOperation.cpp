5719585f326d39c208ab6c8b08f5a8c2a1be6443
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-10 19:02:32 +0600

    splice() function used to transfer elements from one list to another.

--- List/allOperation.cpp
+++ List/allOperation.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int main(){
 
     list<int>li;
+    list<int>li2 {5,2,3,4};
+
     li.assign(5,3);
 
     li.emplace_back(7);
     li.emplace_front(2);
 
     li.pop_front();
+
+    li.splice(li.begin(),li2);
     
     for(int l: li){
         cout << l <<" ";
