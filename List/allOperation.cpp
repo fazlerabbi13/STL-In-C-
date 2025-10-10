c60b930aaed798cde45b6f8ac5594df201dbfe4c
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-10 19:30:52 +0600

    Returns a reverse iterator pointing to the theoretical element preceding the first element in the list container (which is considered its reverse end).

--- List/allOperation.cpp
+++ List/allOperation.cpp
@@ -11,29 +11,31 @@ int main(){
     list<int>li2 {5,2,2,2,3,4};
 
 
-    li2.unique();
+    // li2.unique();
     // li2.remove(4);
 
     // li.assign(5,3);
 
     //  li2.remove_if(single_disit);
 
-    li.emplace_back(7);
-    li.emplace_front(2);
+    // li.emplace_back(7);
+    // li.emplace_front(2);
 
-    li.pop_front();
+    // li.pop_front();
 
     // li.splice(li.begin(),li2);
 
-    
+    for(auto it = li2.rbegin(); it != li2.rend(); ++it){
+        cout << *it << " ";
+    }
 
     // for(int l: li){
     //     cout << l <<" ";
     // }
 
-    for(int l2: li2){
-        cout << l2 <<" ";
-    }
+    // for(int l2: li2){
+    //     cout << l2 <<" ";
+    // }
 
 
     return 0;
