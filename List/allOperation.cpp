ff4fe8f3ade9b1088eb7d70d095d086d01339779
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-10 19:18:34 +0600

    unique() function removes all but the first element from every consecutive group of equal elements in the container.

--- List/allOperation.cpp
+++ List/allOperation.cpp
@@ -8,25 +8,31 @@ bool single_disit(const int& val){
 int main(){
 
     list<int>li;
-    list<int>li2 {5,2,3,4};
+    list<int>li2 {5,2,2,2,3,4};
 
-    li2.remove(4);
+
+    li2.unique();
+    // li2.remove(4);
 
     // li.assign(5,3);
-    
-    li2.remove_if(single_disit);
+
+    //  li2.remove_if(single_disit);
 
     li.emplace_back(7);
     li.emplace_front(2);
 
     li.pop_front();
 
-    li.splice(li.begin(),li2);
+    // li.splice(li.begin(),li2);
 
     
 
-    for(int l: li){
-        cout << l <<" ";
+    // for(int l: li){
+    //     cout << l <<" ";
+    // }
+
+    for(int l2: li2){
+        cout << l2 <<" ";
     }
 
 
