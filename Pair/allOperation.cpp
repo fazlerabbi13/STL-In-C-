89849a36eeeba43735d8e4c44ca1ff14d388a0c3
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-11 19:40:16 +0600

    Exchanges the values of p1 and p2.

--- Pair/allOperation.cpp
+++ Pair/allOperation.cpp
@@ -1,8 +1,12 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
 
 int main(){
-    pair<int,char> p1 (10,'a');
-    cout << p1.first << " : " << p1.second;
+    pair<int,string> p1(10, "abc");      // Changed 'abc' to "abc"
+    pair<int,string> p2(5, "hello");     // Changed 'hello' to "hello"
+
+    p1.swap(p2);
+
+    cout << p1.second;  // Output: hello
     return 0;
 }
