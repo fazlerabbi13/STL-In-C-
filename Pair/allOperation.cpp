33718f10de59567c184281a95a17d4db1ada722d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-11 19:59:47 +0600

    Structured bindings let you split a pair into two separate variables in one step, (instead of using .first and .second).

--- Pair/allOperation.cpp
+++ Pair/allOperation.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 int main(){
-    pair<int,string> p1(10, "abc");      // Changed 'abc' to "abc"
-    pair<int,string> p2(5, "hello");     // Changed 'hello' to "hello"
+    // pair<int,string> p1(10, "abc");      // Changed 'abc' to "abc"
+    // pair<int,string> p2(5, "hello"); 
+    // Changed 'hello' to "hello"
 
-    p1.swap(p2);
+    pair < int , string > myPair = {2,"hello woeld"};
 
-    cout << p1.second;  // Output: hello
+    auto [num, text] = myPair;
+
+    cout << "number: " << num << "\n";
+    cout << "number: " << text << "\n";
+
+    // cout << p1.first << " : " << p1.second <<"\n";
+    // cout << p2.first << " : "<< p2.second <<"\n";
+
+    // p1.swap(p2);
+
+    // cout << p1.second;  // Output: hello
     return 0;
 }
