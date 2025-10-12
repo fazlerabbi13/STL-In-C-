7ec1124601a296641525a9669f513c921d01a7ba
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-12 20:09:28 +0600

    emplace_hint() nserts the key and its element in the map container with a given hint.

--- Map/allOperation.cpp
+++ Map/allOperation.cpp
@@ -7,7 +7,8 @@ int main(){
 
     map1.insert({3,"Fazle Rabbi"});
     map1.insert({5,"Anayet"});
-
+    map1.emplace_hint(map1.begin(),7,"helooo");
+    
     for(auto& element : map1){
         cout << element.first << " " << element.second << "\n";
     }
